2509c77e3044b80682595b77839954cdbb49abc7
nshosain <41326898+nshosain@users.noreply.github.com>
2021-01-22 13:03:52 +0600

    Update main.py

--- main.py
+++ main.py
@@ -11,28 +11,31 @@ r = requests.get(article)
 #get html code
 soup = BeautifulSoup(r.text, 'html.parser')
 
-#get's title of the article
+#gets title of the article
 ttl = soup.find('h1',  attrs={'itemprop': 'headline'})
 title = ttl.string
 
-#get's posted/edited time of the article
+#gets posted/edited time of the article
 tme = soup.find('div', attrs={'class': 'small-text'})
 time = tme.text
 
-#get's body of the article
+#gets author of the article
+athr = soup.find('div', attrs={'class': 'author-name margin-bottom-big'})
+author = athr.text
+
+#gets body of the article
 bdy = soup.find('div', attrs={'class': 'field-body view-mode-full'})
 body = bdy.text
 
 #array for the data collected
 records = []
-records.append((title, time, body))
+records.append((title, time, author, body))
 print('Article Crawl Successful.')
 
 #writes data to the csv file
 import pandas as pd
-df = pd.DataFrame(records, columns=['title', 'time', 'body'])
+df = pd.DataFrame(records, columns=['title', 'time', 'author', 'body'])
 df.to_csv('DailyStar_ArticleDetails.csv', index=False, encoding='utf-8')
 
 #confirmation
 print('Data Write Successful')
-
